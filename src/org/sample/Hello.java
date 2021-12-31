1214ece809b075d176d8a207a8f641bb2978a130
VishnpriyaRavi1028 <vishnuravi8996@gmail.com>
2021-12-31 16:59:01 +0530

    username

--- src/org/sample/Hello.java
+++ src/org/sample/Hello.java
@@ -1,5 +1,12 @@
 package org.sample;
 
 public class Hello {
+	public static void username() {
+		System.out.println("username");
+
+	}
+	public static void main(String[] args) {
+		username();
+	}
 
 }
