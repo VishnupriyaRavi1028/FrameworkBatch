b375c94d7a427de1995afed02b031e69e3c17c72
VishnpriyaRavi1028 <vishnuravi8996@gmail.com>
2021-12-31 17:04:59 +0530

    password

--- src/org/sample/Hello.java
+++ src/org/sample/Hello.java
@@ -1,5 +1,12 @@
 package org.sample;
 
 public class Hello {
+	public static  void password() {
+		System.out.println("password");
+
+	}
+	public static void main(String[] args) {
+		password();
+	}
 
 }
